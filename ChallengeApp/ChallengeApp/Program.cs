22622ca64e680d43a59bb7f0974306bb70d40ede
MateuszLucak <lucak.mateusz@gmail.com>
2023-04-02 19:29:23 +0100

    Challenge21Day4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -6,11 +6,11 @@ if (sex == "female" && name == "Ewa" && age == 30)
 {
      //Console.WriteLine("Ewa lat 30");
 }
-if (sex == "female" || age < 29)
+if (!(sex == "female" && age < 29))
 {
     Console.WriteLine("Niepełnoletni mężczyzna.");
 }
 if (sex == "female" && age > 24)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    //Console.WriteLine("Kobieta poniżej 30 lat");
 }
