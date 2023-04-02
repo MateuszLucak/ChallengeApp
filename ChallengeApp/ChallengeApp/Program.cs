69d8dc0ea4fad8385d34b4d3f18a7a475f014a59
MateuszLucak <lucak.mateusz@gmail.com>
2023-04-02 19:13:31 +0100

    Challenge21Day4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String name = "Ewa";
+String sex = "female";
+var age = 30;
+
+if (sex == "female" && name == "Ewa" && age == 30)
+{
+     //Console.WriteLine("Ewa lat 30");
+}
+if (sex == "female" || age < 29)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+if (sex == "female" && age > 24)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
